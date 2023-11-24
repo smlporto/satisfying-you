5e6e9bff80c2fc94330302310852b9431ade2bca
Welyson <53984581+welyson1@users.noreply.github.com>
2023-11-24 01:23:28 -0300

    adicionado grafico

--- src/screens/Relatorio.js
+++ src/screens/Relatorio.js
@@ -1,41 +1,54 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import PieChart from 'react-native-pie-chart'
 
 const Relatorio = () => {
+    
+    const widthAndHeight = 300
+    const series = [5, 5, 5, 5, 5]
+    const sliceColor = ['#FF0000', '#FF5733', '#FFC300', '#33FF57', '#00FF00']
+
     return (
         <View style={styles.view}>
-            {/* Imagem */}
-            <Image style={styles.imagem} source={require('../../assets/img/grafico.png')}/>
 
-            {/* Container de legendas */}
+            {/* Pie Chart */}
+            <PieChart
+                widthAndHeight={widthAndHeight}
+                series={series}
+                sliceColor={sliceColor}
+            />
+
             <View style={styles.containerList}>
-                <View style={styles.data}>
-                    <View style={styles.square1}/>
-                    <Text style={styles.opinionList}>Excelente</Text>
+
+                <View style={styles.containerItem}>
+                    <View style={styles.square1} />
+                    <Text style={styles.textoLegenda}>Pessimo</Text>
                 </View>
 
-                <View style={styles.data}>
-                    <View style={styles.square2}/>
-                    <Text style={styles.opinionList}>Bom</Text>
+                <View style={styles.containerItem}>
+                    <View style={styles.square2} />
+                    <Text style={styles.textoLegenda}>Ruim</Text>
                 </View>
 
-                <View style={styles.data}>
-                    <View style={styles.square3}/>
-                    <Text style={styles.opinionList}>Neutro</Text>
+                <View style={styles.containerItem}>
+                    <View style={styles.square3} />
+                    <Text style={styles.textoLegenda}>Neutro</Text>
                 </View>
 
-                <View style={styles.data}>
-                    <View style={styles.square4}/>
-                    <Text style={styles.opinionList}>Ruim</Text>
+                <View style={styles.containerItem}>
+                    <View style={styles.square4} />
+                    <Text style={styles.textoLegenda}>Bom</Text>
                 </View>
 
-                <View style={styles.data}>
-                    <View style={styles.square5}/>
-                    <Text style={styles.opinionList}>Péssimo</Text>
+                <View style={styles.containerItem}>
+                    <View style={styles.square5} />
+                    <Text style={styles.textoLegenda}>Excelente</Text>
                 </View>
+
             </View>
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     view: {
@@ -44,52 +57,48 @@ const styles = StyleSheet.create({
         backgroundColor: '#372775',
         padding: 20,
     },
-    imagem: {
-        width: 300,
-        height: 300
-    },
-    containerList: {
-        paddingTop: 54
-    },
-    opinionList: {
-        fontSize: 36,
+    textoLegenda: {
+        fontSize: 24,
         color: '#ffffff',
         fontFamily: 'AveriaLibre-Regular',
     },
-    square1: {
-        width: 30,
-        height: 30,
-        backgroundColor: '#F1CE7E',
-        marginRight: 15,
-    },
-    square2: {
-        width: 30,
-        height: 30,
-        backgroundColor: '#6994FE',
-        marginRight: 15,
-    },
-    square3: {
-        width: 30,
-        height: 30,
-        backgroundColor: '#5FCDA4',
-        marginRight: 15,
-    },
-    square4: {
-        width: 30,
-        height: 30,
-        backgroundColor: '#EA7288',
-        marginRight: 15,
-    },
-    square5: {
-        width: 30,
-        height: 30,
-        backgroundColor: '#53D8D8',
-        marginRight: 15,
-    },
-    data: {
+    containerItem: {
         flexDirection: 'row',
         alignItems: 'center',
     },
-})
+    square1: {
+        width: 20,
+        height: 20,
+        backgroundColor: '#FF0000',
+        marginRight: 15,
+    },
+    square2: {
+        width: 20,
+        height: 20,
+        backgroundColor: '#FF5733',
+        marginRight: 15,
+    },
+    square3: {
+        width: 20,
+        height: 20,
+        backgroundColor: '#FFC300',
+        marginRight: 15,
+    },
+    square4: {
+        width: 20,
+        height: 20,
+        backgroundColor: '#33FF57',
+        marginRight: 15,
+    },
+    square5: {
+        width: 20,
+        height: 20,
+        backgroundColor: '#00FF00',
+        marginRight: 15,
+    },
+    containerList: {
+        marginTop: 20,
+    },
+});
 
 export default Relatorio;
